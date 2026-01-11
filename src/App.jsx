c9d0dbbcc9778e6756ead7af2db5af81f9c7f905
Boyan <bdimosky35@yahoo.com>
2026-01-11 11:35:46 +0100

    Implement First Video

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
-import gsap from "gsap"
-import { ScrollTrigger } from "gsap/all"
-import Hero from "./sections/Hero"
-import Navbar from "./sections/Navbar"
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import Hero from "./sections/Hero";
+import Navbar from "./sections/Navbar";
+import FirstVideo from "./sections/FirstVideo";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
-    return (
-        <main>
-            <Navbar />
-            <Hero />
-        </main>
-    )
-}
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <FirstVideo />
+    </main>
+  );
+};
 
-export default App
+export default App;
